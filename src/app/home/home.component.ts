abff3307993b73bc88626c76b0586a5b3ee20dcc
Karan Sharma <cserkaran@gmail.com>
2017-12-09 15:27:45 +1100

    Finished module on HTTP Form Posting and Data Access

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import { Employee } from '../models/employee.model';
 import { validateConfig } from '@angular/router/src/config';
+import { FormPoster } from '../services/form-poster-service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -8,9 +10,32 @@ import { validateConfig } from '@angular/router/src/config';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  languages = ["English","Spanish","Other"];
-  model = new Employee('','Sharma',true,'w2','default');
+  languages = [];
+  model = new Employee('','',false,'','default');
   hasPrimaryLanguageError = false;
+
+  constructor(private formPoster : FormPoster){
+    this.formPoster.getLanguages()
+        .subscribe(
+          data => this.languages = data.languages,
+          error => console.log('get error: ', error)
+        )
+  }
+
+  submitForm(form:NgForm){
+    //validate form
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+
+    if(this.hasPrimaryLanguageError){
+      return;
+    }
+
+    this.formPoster.postEmployeeForm(this.model)
+        .subscribe(
+          data => console.log('success: ',data),
+          err => console.log('error: ',err)
+        );
+  }
 
   validatePrimaryLanguage(value){
    if(value === 'default'){
